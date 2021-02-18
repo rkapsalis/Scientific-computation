1a71f36dd5a002906347eafada71ab7dea0e4248
Romanos Kapsalis <48930309+rkapsalis@users.noreply.github.com>
2021-02-19 01:14:45 +0200

    Update erotima5_1.m

--- erotima5_1.m
+++ erotima5_1.m
@@ -1,22 +1,18 @@
+%Author: Ρ.ΚΑΨΑΛΗΣ, ΑΜ 1056289, Date:15/02/2021
+%-----------------erotima 5.1.1----------------------
 n=500; 
 A = spdiags((1:n)',(0),n,n);
 xsol = ones(n,1);
 b = A*xsol;
 
 [x,fl0,rr0,it0,r] = pcg(A,b,1e-6,4*n,[],[],zeros(n,1));
-if(fl0==0)
-    disp("yes")
-end
 
+%-----------------erotima 5.1.2----------------------
 n=500;
-A = spdiags([linspace(1,2,n/2)';linspace(1000,1001,n/2)'],(0),n,n);
+C = spdiags([linspace(1,2,n/2)';linspace(1000,1001,n/2)'],(0),n,n);
 xsol = ones(n,1); 
-b = A*xsol;
-
-[x1,fl1,rr1,it1,r1] = pcg(A,b,1e-6,4*n,[],[],zeros(n,1));
-if(fl0==0)
-    disp("yes")
-end
+b1 = C*xsol;
+[x1,fl1,rr1,it1,r1] = pcg(C,b1,1e-6,4*n,[],[],zeros(n,1));
 
 yline(1e-6,'r--');
 
@@ -24,15 +20,11 @@ set(0, 'DefaultAxesFontSize', 18);
 set(0, 'DefaultLineLineWidth', 2);
 
 figure;
-% str = {'Straight Line Plot','from 1 to 10'};
-% annotation('textbox',dim,'String',str,'FitBoxToText','on');
-semilogy(0:length(r)-1,r/norm(2),'bo-'); hold on;
-semilogy(0:length(r1)-1,r1/norm(2),'g-'); hold on;
-txt = {'Norm 2 of residual error:',rr0};
-text(75,1000,txt,'FontSize',14)
-l  = norm(rr0,2);
+semilogy(0:length(r)-1,r/norm(b),'bo-'); hold on;
+semilogy(0:length(r1)-1,r1/norm(b1),'gs-'); hold on;
+
 yline(1e-6,'r--');
 
-legend('No preconditioner');
+legend('A =spdiags([1:n]’,[0],n,n','A =spdiags([linspace(1,2,n/2)’;linspace(1000,1001,n/2)’],[0],n,n','tolerance');
 xlabel('Iteration number')
 ylabel('Relative residual')
